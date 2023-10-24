183b1ea2abe0da5e309ee2dde17c05009e2be463
pej4303 <pej4303@gmail.com>
2023-10-24 22:43:21 +0900

    동기화

--- source/spring-jpa/src/main/java/repository/EmployeeJpaRepository.java
+++ source/spring-jpa/src/main/java/repository/EmployeeJpaRepository.java
@@ -1,24 +1,11 @@
 package repository;
 
 import domain.Employee;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.FluentQuery;
-import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
 /**
  * 스프링 데이터 JPA를 이용한 Repository
  */
-@Repository
 public interface EmployeeJpaRepository extends JpaRepository<Employee, Long> {
 
 }
--- source/spring-jpa/src/test/java/Ch08Test.java
+++ source/spring-jpa/src/test/java/Ch08Test.java
@@ -1,6 +1,7 @@
 import domain.Employee;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,23 +16,35 @@ import repository.EmployeeJpaRepository;
 import java.util.Collections;
 import java.util.List;
 
-
-@SpringBootTest
-@ExtendWith(SpringExtension.class)
+/*
+ * @DataJpaTest :  @Entiy와 스프링 데이터 JPA 레파지토리를 설정함
+ */
+//@DataJpaTest
+@SpringBootTest(classes = Ch08Test.class)
 class Ch08Test {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired
     private EmployeeJpaRepository employeeJpaRepository;
+
+    private Employee employee, employee2;
+
+    /**
+     * 테스트 메서드 이전에 수행함
+     */
+    @BeforeEach
+    void loadData() {
+        employee = new Employee();
+        employee.setName("테스트");
+
+        employee2 = new Employee();
+        employee2.setName("테스트2");
+
+        employeeJpaRepository.saveAll(List.of(employee, employee2));
+    }
 
     @Test
     @DisplayName("스프링 데이터 JPA 사용")
     void test1() {
-        Employee employee = new Employee();
-        employee.setName("스프링데이터JPA");
-
-        Employee saveEntiy = employeeJpaRepository.save(employee);
-
-        Assertions.assertEquals("스프링데이터JPA", saveEntiy.getName());
+        Assertions.assertEquals(List.of(employee, employee2), employeeJpaRepository.findAll());
     }
 }
